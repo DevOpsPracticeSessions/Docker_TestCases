2a08c423f117859ec9a2c8d51e8aa0bf2d0b8a24
sundararajanr <rsundarmail77@gmail.com>
2018-08-07 20:03:50 +0530

    Update param.sh

--- param.sh
+++ param.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo value of parameter is $FromDate $ToDate
-D1=`date +%s -d "2007-12-19"`
-D2=`date +%s -d "2008-02-09"`
-echo ((diff_sec=D2-D1))
+start_date=$(date -j -f "%Y-%m-%d" "2010-05-15" "+%s")
+end_date=$(date -j -f "%Y-%m-%d" "2010-06-01" "+%s")
+echo $(( ($end_date - $start_date) / (60 * 60 * 24) ))

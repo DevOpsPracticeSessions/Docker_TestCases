f77ddfb1f0d02e2220efccd7119327c427942a6b
sundararajanr <rsundarmail77@gmail.com>
2018-08-08 20:24:05 +0530

    Update param.sh

--- param.sh
+++ param.sh
@@ -2,7 +2,7 @@
 echo value of parameter is $FromDate $ToDate
 days=$((($(date -u -d $ToDate +%s) - $(date -u -d $FromDate +%s)) / 86400))
 echo $days
-for ((i=1;i<days;i++))
+for ((i=1;i<3;i++))
        do
            echo "Number $i"
        done
